35e9f6e754521d3139adb0ec64dd2aefeefd388f
jimenezmargarita447-del <jimenezmargarita447@gmail.com>
2026-09-20 22:08:09 -0500

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,29 @@
+basic.forever(function () {
+    if (input.compassHeading() == 270) {
+        basic.showLeds(`
+            . . # . .
+            . # . . .
+            # # # # #
+            . # . . .
+            . . # . .
+            `)
+    } else if (input.compassHeading() == 90) {
+        basic.showLeds(`
+            . . # . .
+            . . . # .
+            # # # # #
+            . . . # .
+            . . # . .
+            `)
+    } else if (input.compassHeading() == 0) {
+        basic.showLeds(`
+            # # # # #
+            # . . . #
+            # . # . #
+            # . . . #
+            # # # # #
+            `)
+    } else {
+    	
+    }
+})
